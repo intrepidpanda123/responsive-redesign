8ec780de301beefe2c8aaa65473a7f1f838f8c37
Intrepid Panda <zachary_zhu+cs1300@brown.edu>
2022-12-07 07:38:37 -0500

    Initial commit

---
+++ arrows.js
@@ -0,0 +1,18 @@
+document.getElementById('biography-collapse').addEventListener('hidden.bs.collapse', () => {
+    document.getElementById('biography-arrow').innerHTML = 'keyboard_arrow_down';
+});
+document.getElementById('characteristics-collapse').addEventListener('hidden.bs.collapse', () => {
+    document.getElementById('characteristics-arrow').innerHTML = 'keyboard_arrow_down';
+});
+document.getElementById('biography-collapse').addEventListener('shown.bs.collapse', () => {
+    document.getElementById('biography-arrow').innerHTML = 'keyboard_arrow_up';
+});
+document.getElementById('characteristics-collapse').addEventListener('shown.bs.collapse', () => {
+    document.getElementById('characteristics-arrow').innerHTML = 'keyboard_arrow_up';
+});
+document.getElementById('table-of-contents-collapse').addEventListener('hidden.bs.collapse', () => {
+    document.getElementById('table-of-contents-arrow').innerHTML = 'keyboard_arrow_down';
+});
+document.getElementById('table-of-contents-collapse').addEventListener('shown.bs.collapse', () => {
+    document.getElementById('table-of-contents-arrow').innerHTML = 'keyboard_arrow_up';
+});
---
+++ scripts.js
@@ -0,0 +1,74 @@
+function expandAndScrollIn(toExpand, toScroll) {
+  if (!(toExpand === '')) {
+    const collapsible = document.getElementById(toExpand);
+    try {
+      const scroll = () => {scrollIn(toScroll)};
+      collapsible.addEventListener('shown.bs.collapse', scroll, { once: true });
+      collapsible.addEventListener('hidden.bs.collapse', () => {
+        collapsible.removeEventListener('shown.bs.collapse', scroll);
+      });
+      bootstrap.Collapse.getInstance(collapsible).show();
+    } catch (e) {
+      scrollIn(toScroll);
+    }
+  }
+  scrollIn(toScroll);
+}
+
+function scrollIn(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: 'auto',
+  })
+}
+
+function updateTableOfContentsHighlight() {
+  const scroll = $(document).scrollTop();
+  let highlight_id = null;
+  $('h1, h2, h3, h4, h5, h6').each((_, header) => {
+    const current_element = header;
+    const toc_id = '#toc_' + $(current_element).attr('id');
+    const elem_ypos = $(current_element).offset().top;
+    if (elem_ypos < scroll + 108) {
+      highlight_id = toc_id;
+    }
+    if ($(toc_id).length > 0) {
+      $(toc_id).removeClass('active');
+    }
+  });
+  // only highlight *last* element that is above the window's scroll position.
+  if ($(highlight_id).length > 0) {
+    $(highlight_id).addClass('active');
+  }
+}
+
+window.onscroll = function() {updateTableOfContentsHighlight()};
+
+function hideAdBanner() {
+  document.getElementById('banner').hidden = true;
+}
+
+window.outlineOpen = false;
+
+function toggleOutline() {
+  window.outlineOpen = !window.outlineOpen;
+  if (outlineOpen) {
+    document.getElementById('outline-container').style.display = 'flex';
+  } else {
+    document.getElementById('outline-container').style.display = 'none';
+  }
+}
+
+function toggleSearch() {
+  const searchIcon = document.getElementById('search-icon');
+  if (searchIcon.innerHTML === 'search') {
+    document.getElementById('search-bar').style.display = 'inline';
+    document.getElementById('wookie-logo').style.display = 'none';
+    document.getElementById('wookie').style.display = 'none';
+    searchIcon.innerHTML = 'close';
+  } else {
+    document.getElementById('search-bar').style.display = 'none';
+    document.getElementById('wookie-logo').style.display = 'inline';
+    document.getElementById('wookie').style.display = 'inline';
+    searchIcon.innerHTML = 'search';
+  }
+}
